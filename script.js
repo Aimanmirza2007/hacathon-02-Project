b7a82dd82769ef59b157d112d318abb7ad6d684a
Aimanmirza2007 <sm501623@gmail.com>
2025-04-12 14:56:17 +0500

    first commit

--- script.js
+++ script.js
@@ -9,72 +9,7 @@ Login.addEventListener('click',function(){
   window.location.href = "./signuplogin folder/login.html"
 })
 
-// Navbar toggle
 
-
-
-// const navLinks = document.getElementById("nav-links");
-// const newMenu = document.getElementById("nav-menu");
-// const newClose = document.getElementById("nav-close");
-
-// if (newMenu) {
-//   newMenu.addEventListener("click", () => {
-//     navLinks.classList.add("show-menu");
-//   });
-// }
-
-// if (newClose) {
-//   newClose.addEventListener("click", () => {
-//     navLinks.classList.remove("show-menu");
-//   });
-// }
-// cards work
-
-
-//  const add = document.getElementById("add");
-//   add.addEventListener('click',function(){
-//     const title = document.getElementById("Title").value
-//     const description = document.getElementById("description").value;
-//     const category = document.getElementById("category").value;
-
-//     const card = document.createElement('div');
-//     card.classList.add('card');
-  
-//     card.innerHTML = `
-//       <h3>${title}</h3>
-//       <p>${description}</p> 
-//       <small>Category: ${category}</small>
-//     `;
-//     document.getElementById('cardsContainer').appendChild(card);
-
-//     // Inputs clear karne ke liye
-//     document.getElementById('title').value = '';
-//     document.getElementById('description').value = '';
-//     document.getElementById('category').value = '';
-//   })
-
-// function add(){
-//     const title = document.getElementById("Title").value
-//         const description = document.getElementById("description").value;
-//         const category = document.getElementById("category").value;
-    
-//         const card = document.createElement('div');
-//         card.classList.add('card');
-      
-//         card.innerHTML = `
-//           <h3>${title}</h3>
-//           <p>${description}</p> 
-//           <small>Category: ${category}</small>
-//         `;
-//         document.getElementById('cardsContainer').appendChild(card);
-    
-//         // Inputs clear karne ke liye
-//         document.getElementById('title').value = '';
-//         document.getElementById('description').value = '';
-//         document.getElementById('category').value = '';
-      
-    
-// }
 
 document.getElementById('addButton').addEventListener('click', function() {
     
@@ -90,8 +25,8 @@ document.getElementById('addButton').addEventListener('click', function() {
       
       card.innerHTML = `
         <h4>${title}</h4>
-        <p>${description}</p>
-        <div class="date">${category}</div>
+        <p><strong>Description</strong>${description}</p>
+        <div class="category"><strong>Description</strong>${category}</div>
       `;
       
       
